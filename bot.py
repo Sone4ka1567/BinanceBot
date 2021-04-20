8b35f1c1c7119d92513374e3ced6a524b1c52309
Son7bon <sone4ka1567@gmail.com>
2021-04-20 23:23:24 +0300

    :zap: Add processing callbacks

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from config import TOKEN
 import keyboards as kb
 
 
-# logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
@@ -14,12 +14,12 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
-    await message.reply("Привет!\nГотов заглянуть на рынок крипты?")
+    await bot.send_message(message.from_user.id, text="Привет!\nГотов заглянуть на рынок крипты?")
 
 
 @dp.message_handler(commands=['help'])
 async def process_help_command(message: types.Message):
-    await message.reply("Напиши мне /currency и выбери криптовалюту, я сообщу тебе её стоимость в данный момент!")
+    await bot.send_message(message.from_user.id, text="Напиши мне /currency и выбери криптовалюту, я сообщу тебе её стоимость в данный момент!")
 
 
 #@dp.message_handler()
@@ -27,9 +27,24 @@ async def process_help_command(message: types.Message):
 #    await bot.send_message(msg.from_user.id, msg.text)
 
 
+@dp.callback_query_handler(lambda c: c.data)
+async def process_callback_keyboard(callback_query: types.CallbackQuery):
+    code = callback_query.data
+    if code == 'btc':
+        await bot.answer_callback_query(callback_query.id, text='BTC')
+    elif code == 'bnb':
+        await bot.answer_callback_query(callback_query.id, text='BNB')
+    elif code == 'eth':
+        await bot.answer_callback_query(callback_query.id, text='ETH')
+    elif code == 'ltc':
+        await bot.answer_callback_query(callback_query.id, text='LTC')
+    elif code == 'usdt':
+        await bot.answer_callback_query(callback_query.id, text='USDT')
+
+
 @dp.message_handler(commands=['currency'])
 async def process_command_list(message: types.Message):
-    await message.reply("Выбери крипту :)", reply_markup=kb.inline_keyboard)
+    await bot.send_message(message.from_user.id, text="Выбери крипту :)", reply_markup=kb.inline_keyboard)
 
 
 if __name__ == '__main__':
