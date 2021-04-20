3bd3b7cc409728c1a5304321b04f784786e0dae0
Son7bon <sone4ka1567@gmail.com>
2021-04-20 23:59:02 +0300

    :sparkles: Rewrite to english

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ dp = Dispatcher(bot)
 async def process_start_command(message: types.Message):
     await bot.send_message(
         message.from_user.id,
-        text="Привет!\nГотов заглянуть на рынок крипты?"
+        text="Hey!\nAre you ready for the crypto world?)"
     )
 
 
@@ -24,43 +24,49 @@ async def process_start_command(message: types.Message):
 async def process_help_command(message: types.Message):
     await bot.send_message(
         message.from_user.id,
-        text="Напиши мне /currency и выбери криптовалюту,"
-            " я сообщу тебе её стоимость в $ в данный момент!"
+        text="Send /currency and choose crypto,"
+             "I'll tell you its cost in $ at this moment!"
     )
 
 
-#@dp.message_handler()
-#async def echo_message(msg: types.Message):
+# @dp.message_handler()
+# async def echo_message(msg: types.Message):
 #    await bot.send_message(msg.from_user.id, msg.text)
 
 
 @dp.callback_query_handler(lambda c: c.data)
-async def process_callback_keyboard(callback_query: types.CallbackQuery):
+async def process_callback_keyboard(
+        callback_query: types.CallbackQuery):
     code = callback_query.data
     if code == 'btc':
         await bot.send_message(
             callback_query.from_user.id,
-            text=('BTC is worth ' + str(get_current_price('BTCUSDT') + '$'))
+            text=('BTC is worth ' + str(
+                get_current_price('BTCUSDT') + '$'))
         )
     elif code == 'bnb':
         await bot.send_message(
             callback_query.from_user.id,
-            text=('BNB is worth ' + str(get_current_price('BNBUSDT') + '$'))
+            text=('BNB is worth ' + str(
+                get_current_price('BNBUSDT') + '$'))
         )
     elif code == 'eth':
         await bot.send_message(
             callback_query.from_user.id,
-            text=('ETH is worth ' + str(get_current_price('ETHUSDT') + '$'))
+            text=('ETH is worth ' + str(
+                get_current_price('ETHUSDT') + '$'))
         )
     elif code == 'ltc':
         await bot.send_message(
             callback_query.from_user.id,
-            text=('LTC is worth ' + str(get_current_price('LTCUSDT') + '$'))
+            text=('LTC is worth ' + str(
+                get_current_price('LTCUSDT') + '$'))
         )
     elif code == 'usdt':
         await bot.send_message(
             callback_query.from_user.id,
-            text=('USDT is worth ' + str(get_current_price('USDTRUB') + '₽'))
+            text=('USDT is worth ' + str(
+                get_current_price('USDTRUB') + '₽'))
         )
 
 
@@ -68,7 +74,7 @@ async def process_callback_keyboard(callback_query: types.CallbackQuery):
 async def process_command_list(message: types.Message):
     await bot.send_message(
         message.from_user.id,
-        text="Выбери крипту, и я скажу её стоимость в долларах :)",
+        text="Choose crypto and I'll tell you its worth in $ :)",
         reply_markup=kb.inline_keyboard
     )
 
