fe65103b604a7e7c83ee52e1c461bc368a49c3fd
Son7bon <sone4ka1567@gmail.com>
2021-04-30 00:53:40 +0300

    :construction: Start work on SQL

--- sql_parser.py
+++ sql_parser.py
@@ -4,6 +4,7 @@ import sqlite3
 class SQLParser:
     def __init__(self):
         self.bd_name = 'orders.db'
+        self.table = 'orders'
         conn = sqlite3.connect(self.bd_name)
         cur = conn.cursor()
         cur.execute("""CREATE TABLE IF NOT EXISTS orders(
@@ -15,8 +16,17 @@ class SQLParser:
         """)
         conn.commit()
 
-    def get_all(self):
-        pass
+    def get_all(self, user_id, columns=('date', 'currency1', 'currency2', 'value')):
+        conn = sqlite3.connect(self.bd_name)
+        cur = conn.cursor()
+        output = []
+        columns_names = ' ,'.join(columns)
+        for num in range(len(columns)):
+            output.append([i[num] for i in cur.execute(f"SELECT {columns_names} "
+                                                       f"FROM {self.table} WHERE "
+                                                       f"userid = '{user_id}'")])
+        conn.close()
+        return output
 
     def add(self, data):
         conn = sqlite3.connect(self.bd_name)
